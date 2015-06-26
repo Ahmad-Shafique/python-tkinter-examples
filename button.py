deb684f47396a8d8196584e7f631319fe0ecd6ab
Andrew Steele <andrew@andrewsteele.me.uk>
2015-06-26 11:04:46 +0100

    Demonstration of Button widget

---
+++ button.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+Showcasing a Button added to a Window, which when clicked, outputs a message to
+the user.
+'''
+
+import tkinter
+
+class Button(tkinter.Tk):
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.title("Button")
+
+        button = tkinter.Button(text="Button", command=self.on_button_click)
+        button.pack(fill=tkinter.BOTH, expand=1)
+
+    def on_button_click(self):
+        print("Button clicked")
+
+if __name__ == "__main__":
+    application = Button()
+    application.mainloop()
