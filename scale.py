f1e951a239ea96d9768007f05d0828f875de1f99
Andrew Steele <andrew@andrewsteele.me.uk>
2015-07-03 19:24:02 +0100

    Include Scale and Spinbox demonstrations

---
+++ scale.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+'''
+Entry example which takes any entered text and displays it when the user
+presses the button.
+'''
+
+import tkinter
+
+class Window(tkinter.Tk):
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.title("Scale")
+
+        scale = tkinter.Scale()
+        scale.config(from_=0, to=100)
+        scale.set(25)
+        scale.pack(fill=tkinter.BOTH, expand=0)
+
+if __name__ == "__main__":
+    application = Window()
+    application.mainloop()
---
+++ spinbox.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+Entry example which takes any entered text and displays it when the user
+presses the button.
+'''
+
+import tkinter
+
+class Window(tkinter.Tk):
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.title("Spinbox")
+
+        self.spinbox = tkinter.Spinbox(command=self.on_spinbox_change)
+        self.spinbox.config(from_=10, to=20)
+        self.spinbox.pack(fill=tkinter.BOTH, expand=0)
+
+    def on_spinbox_change(self):
+        print(self.spinbox.get())
+
+if __name__ == "__main__":
+    application = Window()
+    application.mainloop()
