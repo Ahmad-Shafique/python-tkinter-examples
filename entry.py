c0987a8d1e6d0a51b113d5afa455caee997f91c4
Andrew Steele <andrew@andrewsteele.me.uk>
2015-07-03 19:23:49 +0100

    Addition of Entry example file

---
+++ entry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+'''
+Entry example which takes any entered text and displays it when the user
+presses the button.
+'''
+
+import tkinter
+
+class Window(tkinter.Tk):
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.title("Entry")
+
+        self.entry = tkinter.Entry()
+        self.entry.pack(fill=tkinter.BOTH, expand=0)
+
+        button = tkinter.Button(text="Enter", command=self.on_button_click)
+        button.pack(fill=tkinter.BOTH, expand=0)
+
+    def on_button_click(self):
+        print(self.entry.get())
+
+if __name__ == "__main__":
+    application = Window()
+    application.mainloop()
